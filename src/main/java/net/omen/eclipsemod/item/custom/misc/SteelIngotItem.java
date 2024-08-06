b5329e520548d7bdbf657d9edf553c897c6a486b
OmenV06 <155521972+OmenV06@users.noreply.github.com>
2024-08-06 04:35:27 -0500

    Post tweaks and AirWalkPower. Early version of AbilityBar

---
+++ src/main/java/net/omen/eclipsemod/item/custom/misc/SteelIngotItem.java
@@ -0,0 +1,11 @@
+package net.omen.eclipsemod.item.custom.misc;
+
+import net.minecraft.world.item.Item;
+import net.omen.eclipsemod.item.ModCreativeModeTabs;
+
+public class SteelIngotItem extends Item {
+
+    public SteelIngotItem() {
+        super(new Item.Properties().tab(ModCreativeModeTabs.ECLIPSE_TAB));
+    }
+}
